a969496c2e33e22f4ec919f2fe766671bc3cf5b8
John Lorica <jroderick.0717@gmail.com>
2026-05-25 21:11:51 +0800

    Made files for each Gemini solution

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
+def weight_in_lbs = float()
+
+
+def weight_in_kg = float()
+
+
 weight = input(
     "Type 1 to convert from pounds to kilos or 2 to convert from kilos to pounds: ")
 if weight == "1":
-    print(weight_in_lbs=weight_in_kg * 2.20462)
+    print(weight_in_lbs=float(weight_in_kg * 2.20462))
 elif weight == "2":
-    print(weight_in_kg=weight_in_lbs / 2.20462)
+    print(weight_in_kg=float(weight_in_lbs / 2.20462))
 else:
     print("Invalid, please pick 1 or 2.")
-
-
-weight_in_kg = float(input("Enter weight in KG: "))
-weight_in_lbs = weight_in_kg * 2.20462
-print(weight_in_lbs)
-
-weight_in_lbs = float(input("Enter weight in LBS: "))
-weight_in_kg = weight_in_lbs / 2.20462
-print(weight_in_kg)
---
+++ test-2.py
@@ -0,0 +1,7 @@
+weight_in_kg = float(input("Enter weight in KG: "))
+weight_in_lbs = weight_in_kg * 2.20462
+print(weight_in_lbs)
+
+weight_in_lbs = float(input("Enter weight in LBS: "))
+weight_in_kg = weight_in_lbs / 2.20462
+print(weight_in_kg)
