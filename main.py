a4f16a042b2f485fd612da8e0a4811408ba8b8f2
John Lorica <jroderick.0717@gmail.com>
2026-05-25 16:04:41 +0800

    Trying things out and failing like Linus said

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-# Input and if statements
+def convert_kg_to_lbs(weight_in_kg):
+    result = weight_in_kg * 2.20462
+    return result
+
+
+def convert_lbs_tokg(weight_in_lbs):
+    result = weight_in_lbs / 2.20462
+    return result
+
+
 weight = input(
     "Type 1 to convert from pounds to kilos or 2 to convert from kilos to pounds: ")
 if weight == "1":
-    print(lbs=kg * 2.20462)
+    print(weight_in_lbs=weight_in_kg * 2.20462)
 elif weight == "2":
-    print(kg=lbs / 2.20462)
+    print(weight_in_kg=weight_in_lbs / 2.20462)
 else:
     print("Invalid, please pick 1 or 2.")
 
 
-# Assignment operator
-kilograms = float(input("Enter weight in KG: "))
-pounds = kilograms * 2.20462
-print(pounds)
+weight_in_kg = float(input("Enter weight in KG: "))
+weight_in_lbs = weight_in_kg * 2.20462
+print(weight_in_lbs)
 
-pounds = float(input("Enter weight in LBS: "))
-kilograms = pounds / 2.20462
-print(kilograms)
-
-
-# Definition and function
-def convert_kg_to_lbs(weight_in_kg):
-    result = weight_in_kg * 2.20462
-    return result
+weight_in_lbs = float(input("Enter weight in LBS: "))
+weight_in_kg = weight_in_lbs / 2.20462
+print(weight_in_kg)
 
 
 user_kg = float(input("Enter kilograms to convert: "))
