6385d2f3d00559ee4bb613a83620883334414c72
John Lorica <jroderick.0717@gmail.com>
2026-05-25 21:13:00 +0800

    Removed some comments

--- test-3.py
+++ test-3.py
@@ -1,14 +1,13 @@
-# 1. Ask the user for their choice
 choice = input("Type 1 to convert KG to LBS, or 2 to convert LBS to KG: ")
 
-# 2. Check the choice using an if/elif statement
+
 if choice == "1":
     print("You chose KG to LBS!")
-    # Your conversion math will go here
+
 
 elif choice == "2":
     print("You chose LBS to KG!")
-    # Your conversion math will go here
+
 
 else:
     print("Invalid choice! Please run the program again and type 1 or 2.")
