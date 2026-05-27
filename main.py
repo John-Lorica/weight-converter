3a8d0e50263c54bdf7df54b315258e33f4e19c75
John Lorica <jroderick.0717@gmail.com>
2026-05-27 20:29:36 +0800

    Did a workflow

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ choice = input("Type 1 to convert KG to LBS, or 2 to convert LBS to KG: ")
 if choice == "1":
     print("You chose KG to LBS!")
 
-
 elif choice == "2":
     print("You chose LBS to KG!")
 
---
+++ test-4.py
@@ -0,0 +1,2 @@
+# The others are for the answer Gemini gave and for you to tweak them.
+# This is for you to write things out for youself to how the code works.
