f92d5103eaf59360417cfdb91469f1f024577ebf
John Lorica <jroderick.0717@gmail.com>
2026-05-28 21:26:00 +0800

    I have plans, but not how to put those plans into action

--- main.py
+++ main.py
@@ -10,24 +10,3 @@ elif choice == "2":
 
 else:
     print("Invalid choice! Please run the program again and type 1 or 2.")
-
-
-def convert_kg_to_lbs(weight_in_kg):
-    result = weight_in_kg * 2.20462
-    return result
-
-
-def convert_lbs_to_kg(weight_in_lbs):
-    result = weight_in_lbs / 2.20462
-    return result
-
-
-user_kg = float(input("Enter kilograms to convert: "))
-final_lbs = convert_kg_to_lbs(user_kg)
-print(f"That is equal to {final_lbs} lbs!")
-
-user_lbs = float(input("Enter pounds to convert: "))
-final_kg = convert_lbs_to_kg(user_lbs)
-print(f"That is equal to {final_kg} kg!")
-
-# There needs to be an end here. Like, if you've already got your answer, it just ends.
--- test-4.py
+++ test-4.py
@@ -1,2 +1,22 @@
 # The others are for the answer Gemini gave and for you to tweak them.
 # This is for you to write things out for youself to how the code works.
+
+def convert_kg_to_lbs(weight_in_kg):
+    result = weight_in_kg * 2.20462
+    return result
+
+
+def convert_lbs_to_kg(weight_in_lbs):
+    result = weight_in_lbs / 2.20462
+    return result
+
+
+user_kg = float(input("Enter kilograms to convert: "))
+final_lbs = convert_kg_to_lbs(user_kg)
+print(f"That is equal to {final_lbs} lbs!")
+
+user_lbs = float(input("Enter pounds to convert: "))
+final_kg = convert_lbs_to_kg(user_lbs)
+print(f"That is equal to {final_kg} kg!")
+
+# There needs to be an end here. Like, if you've already got your answer, it just ends.
